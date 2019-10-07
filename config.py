99facee5a2d8a792212cbecf89d23ed2caf2cedb
unknown <bastinda96@gmail.com>
2019-10-08 02:51:37 +0300

    config->cool envvars

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 
-use_proxy = os.environ["use_proxy"] == "True"
-proxy_user = os.environ["proxy_user"]
-proxy_pass = os.environ["proxy_pass"]
-proxy_address = os.environ["proxy_address"]
-proxy_port = int(os.environ["proxy_port"])
-token = os.environ["token"]
+use_proxy = os.environ.get("use_proxy", False) == "True"
+proxy_user = os.environ.get("proxy_user", None)
+proxy_pass = os.environ.get("proxy_pass", None)
+proxy_address = os.environ.get("proxy_address", None)
+proxy_port = int(os.environ.get("proxy_port", 0))
+token = os.environ.get("token", "awahahamybeautifultokenyoucantsteal888")
